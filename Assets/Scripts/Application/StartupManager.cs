a38178d4b087ebc0c0f763308aaac2504d32887f
Patryk Lesiak <patryklesiak@student.agh.edu.pl>
2023-09-30 15:39:57 +0200

    Unity init

---
+++ Assets/Scripts/Application/StartupManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartupManager : MonoBehaviour
+{
+    void Start()
+    {
+        // All non destoryable are now loaded, go to menu
+            
+    }
+}
