537650224449577c11918ba97955eec508e1ddc0
Stanisław Borowy <stabor@startmail.com>
2023-10-01 03:20:18 +0200

    żyrafafafa

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -25,6 +25,6 @@ public class Player : MonoBehaviour {
     }
 
     void Update() {
-        animator.SetFloat("velocity", agent.speed);
+        animator.SetFloat("Velocity", agent.speed);
     }
 }
